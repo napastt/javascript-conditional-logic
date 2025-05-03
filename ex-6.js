3e69a142d17a8333f4c6b8ed28bd4889e45f8427
Napas T. <napassorn.ti@gmail.com>
2025-05-03 14:38:24 +0700

    Modified ex-6

--- ex-6.js
+++ ex-6.js
@@ -2,7 +2,11 @@ let favoriteBooks = [];
 
 function addFavoriteBook(bookName) {
   // Start coding here !
-}
+  if (bookName.includes("Beauty") === true) {
+    favoriteBooks.push(bookName);
+    return favoriteBooks;
+  }
+};
 
 // Example case
 addFavoriteBook("Let Me Tell You What I Mean");
